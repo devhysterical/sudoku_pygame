040e7ff3306564a10fe3379a8535a113f5520cd1
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2024-04-05 14:55:04 +0700

    Update file formatting

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+# import pygame
+# import sys
+# import random
+# import time
+
+# # Khởi tạo Pygame
+# pygame.init()
+
+# # Constants
+# WIDTH, HEIGHT = 540, 590
+# GRID_SIZE = 9
+# CELL_SIZE = WIDTH // GRID_SIZE
+# WHITE = (255, 255, 255)
+# BLACK = (0, 0, 0)
+# FONT = pygame.font.SysFont("comicsans", 40)
+# TIMER_FONT = pygame.font.SysFont("comicsans", 30)
+# TIMER_DURATION = (
+#     60  # Tùy chỉnh bộ đếm thời gian cho trò chơi theo sở thích của người chơi
+# )
